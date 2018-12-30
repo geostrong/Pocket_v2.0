6a614f6bd972df7b62031ad010daa62626461776
Wilson <wilsongoh94@hotmail.com>
2018-12-30 18:09:46 +0800

    added dynamic qr generate

--- app/src/main/java/com/pocketwallet/pocket_v2/RequestFragment_QR.java
+++ app/src/main/java/com/pocketwallet/pocket_v2/RequestFragment_QR.java
@@ -1,5 +1,6 @@
 package com.pocketwallet.pocket_v2;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 
 public class RequestFragment_QR extends Fragment {
@@ -14,6 +24,33 @@ public class RequestFragment_QR extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_request_fragment__qr, container, false);
+
+        //<--Setup image view-->
+        final ImageView generatedQR = (ImageView) view.findViewById(R.id.generatedDynamicQR);
+        //<--End-->
+
+        final EditText amountEntered = (EditText) view.findViewById(R.id.requestAmountQR);
+
+        //<--Setup buttons-->
+        final Button generateQrBtn = (Button) view.findViewById(R.id.generate_qr);
+        generateQrBtn.setOnClickListener(new  View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try {
+                    System.out.println(amountEntered.toString());
+                    BitMatrix bitMatrix = multiFormatWriter.encode(amountEntered.getText().toString(), BarcodeFormat.QR_CODE,generatedQR.getWidth(),generatedQR.getHeight());
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    generatedQR.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        //<---->
+
         return view;
     }
 }
