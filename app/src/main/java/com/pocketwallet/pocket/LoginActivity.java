9fda824af25b5d80bc9b185680ddbdd2fdaaf9d4
Wilson <wilsongoh94@hotmail.com>
2019-01-01 20:55:55 +0800

    removed splash

--- app/src/main/java/com/pocketwallet/pocket/LoginActivity.java
+++ app/src/main/java/com/pocketwallet/pocket/LoginActivity.java
@@ -15,7 +15,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.json.JSONException;
 import org.json.JSONObject;
