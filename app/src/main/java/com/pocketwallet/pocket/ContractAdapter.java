fca1134ed85a0fb57c52a002b11f4af3fd77f055
christiantanputra <christian_tanputra@icloud.com>
2019-01-24 22:13:29 +0800

    added ability to expand and show more information for contract cardview

--- app/src/main/java/com/pocketwallet/pocket/ContractAdapter.java
+++ app/src/main/java/com/pocketwallet/pocket/ContractAdapter.java
@@ -3,6 +3,7 @@ package com.pocketwallet.pocket;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,9 @@ import java.util.List;
 
 
 public class ContractAdapter extends RecyclerView.Adapter<ContractAdapter.ViewHolder> {
+
+    int mExpandedPosition = -1;
+    int previousExpandedPosition = -1;
 
     private List<ListContract> listContracts;
     private Context context;
@@ -30,13 +34,36 @@ public class ContractAdapter extends RecyclerView.Adapter<ContractAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder (ViewHolder holder, int position){
+    public void onBindViewHolder (ViewHolder holder, final int position){
         ListContract listContract = listContracts.get(position);
 
         holder.textViewName.setText(listContract.getName());
         holder.textViewNumber.setText(listContract.getNumber());
         holder.textViewDescription.setText(listContract.getDescription());
         holder.textViewEndDate.setText(listContract.getEndDate());
+
+        final boolean isExpanded = position==mExpandedPosition;
+        holder.textViewDescription.setVisibility(isExpanded?View.GONE:View.VISIBLE);
+        holder.textViewEndDate.setVisibility(isExpanded?View.GONE:View.VISIBLE);
+        holder.textViewDescription2.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+        holder.textViewEndDate2.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+        holder.textViewFeePerFreq.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+        holder.textViewFrequency.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+        holder.textViewPerSign.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+        holder.textViewTerminateContractBtn.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+        holder.itemView.setActivated(isExpanded);
+
+        if (isExpanded)
+            previousExpandedPosition = position;
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mExpandedPosition = isExpanded ? -1:position;
+                notifyItemChanged(previousExpandedPosition);
+                notifyItemChanged(position);
+            }
+        });
     }
 
     @Override
@@ -46,7 +73,7 @@ public class ContractAdapter extends RecyclerView.Adapter<ContractAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView textViewName, textViewNumber, textViewDescription, textViewEndDate;
+        TextView textViewName, textViewNumber, textViewDescription, textViewEndDate, textViewDescription2, textViewEndDate2, textViewFeePerFreq, textViewFrequency, textViewPerSign, textViewTerminateContractBtn;
 
         public ViewHolder(View itemView){
             super(itemView);
@@ -55,6 +82,12 @@ public class ContractAdapter extends RecyclerView.Adapter<ContractAdapter.ViewHo
             textViewNumber = itemView.findViewById(R.id.numberInvolved);
             textViewDescription = itemView.findViewById(R.id.description);
             textViewEndDate = itemView.findViewById(R.id.endDate);
+            textViewDescription2 = itemView.findViewById(R.id.description2);
+            textViewEndDate2 = itemView.findViewById(R.id.endDate2);
+            textViewFeePerFreq = itemView.findViewById(R.id.feePerFreq);
+            textViewFrequency = itemView.findViewById(R.id.frequency);
+            textViewPerSign = itemView.findViewById(R.id.perSign);
+            textViewTerminateContractBtn = itemView.findViewById(R.id.terminateContractBtn);
         }
     }
 }
--- app/src/main/java/com/pocketwallet/pocket/ListContract.java
+++ app/src/main/java/com/pocketwallet/pocket/ListContract.java
@@ -5,12 +5,22 @@ public class ListContract {
     private String numberInvolved;
     private String description;
     private String endDate;
+    private String description2;
+    private String endDate2;
+    private String feePerFreq;
+    private String frequency;
 
-    public ListContract(String nameInvolved, String numberInvolved, String description, String endDate){
+
+
+    public ListContract(String nameInvolved, String numberInvolved, String description, String endDate, String description2, String endDate2, String feePerFreq, String frequency){
         this.nameInvolved = nameInvolved;
         this.numberInvolved = numberInvolved;
         this.description = description;
         this.endDate = endDate;
+        this.description2 = description2;
+        this.endDate2 = endDate2;
+        this.feePerFreq = feePerFreq;
+        this.frequency = frequency;
     }
 
     public String getName(){
@@ -27,5 +37,21 @@ public class ListContract {
 
     public String getEndDate(){
         return endDate;
+    }
+
+    public String getDescription2(){
+        return description2;
+    }
+
+    public String getEndDate2(){
+        return endDate2;
+    }
+
+    public String getFeePerFreq(){
+        return feePerFreq;
+    }
+
+    public String getFrequency(){
+        return frequency;
     }
 }
