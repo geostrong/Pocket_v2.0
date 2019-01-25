21b79717da96ac3427cf2774bfb4e19bad530f47
Yy <cyy0120@hotmail.com>
2019-01-26 02:15:03 +0800

    Transaction Editted

--- app/src/main/java/com/pocketwallet/pocket/Transaction.java
+++ app/src/main/java/com/pocketwallet/pocket/Transaction.java
@@ -1,6 +1,8 @@
 package com.pocketwallet.pocket;
 
-public class ListTransaction {
+import java.util.Date;
+
+public class Transaction {
     private String nameInvolved;
     private String numberInvolved;
     private String transactAmount;
@@ -8,17 +10,30 @@ public class ListTransaction {
     private String transactDate;
     private String transactTime;
 
-    public ListTransaction(){
+    private String transactionID;
+    private String type;
+    private String origin;
+    private String destination;
+    private double amount;
+    private Date timestamp;
 
-    }
-
-    public ListTransaction (String nameInvolved, String numberInvolved, String transactAmount, String transactID, String transactDate, String transactTime){
+    public Transaction (String nameInvolved, String numberInvolved, String transactAmount, String transactID, String transactDate, String transactTime){
         this.nameInvolved = nameInvolved;
         this.numberInvolved = numberInvolved;
         this.transactAmount = transactAmount;
         this.transactID = transactID;
         this.transactDate = transactDate;
         this.transactTime = transactTime;
+    }
+
+    public Transaction(String transactionRefID, String type, String senderID, String receiverID, double amount,
+                       Date date){
+        this.transactionID = transactionRefID;
+        this.type = type;
+        this.origin = senderID;
+        this.destination = receiverID;
+        this.amount = amount;
+        this.timestamp = date;
     }
 
     public String getName(){
--- app/src/main/java/com/pocketwallet/pocket/TransactionLogsActivity.java
+++ app/src/main/java/com/pocketwallet/pocket/TransactionLogsActivity.java
@@ -9,17 +9,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
 
 public class TransactionLogsActivity extends AppCompatActivity {
 
     private RecyclerView transactionListView;
     private RecyclerView.Adapter adapter;
 
-    private List<ListTransaction> listTransactions;
+    private List<Transaction> listTransactions;
+    private ArrayList transactionsArrayList;
+
+    private String userId;
+    private String urlRetrieveTransactionHistory = "http://pocket.ap-southeast-1.elasticbeanstalk.com/transactional/transactionhistory";
+
+    Bundle extras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +60,7 @@ public class TransactionLogsActivity extends AppCompatActivity {
         listTransactions = new ArrayList<>();
 
         for(int i=0; i<=9; i++){
-            ListTransaction listTransaction = new ListTransaction(
+            Transaction listTransaction = new Transaction(
                     "Harold" + i,
                     "1234 5678",
                     "-$100",
@@ -43,12 +70,29 @@ public class TransactionLogsActivity extends AppCompatActivity {
             );
 
             listTransactions.add(listTransaction);
+
+            extras = getIntent().getExtras();
+            if (extras != null) {
+                userId = extras.getString("userId");
+            }
         }
+        GetTransactions();
 
-        adapter = new TransactAdapter(listTransactions,this);
-
-        transactionListView.setAdapter(adapter);
+        //adapter = new TransactAdapter(listTransactions,this);
+        //transactionListView.setAdapter(adapter);
     }
 
+    public void GetTransactions(){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        try {
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("user_id", userId);
+            System.out.println("TEST PRINTING: " + jsonBody);
+
+            //requestQueue.add(jsonObjectRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
