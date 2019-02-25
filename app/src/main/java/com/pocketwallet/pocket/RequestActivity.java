6d4b35b5fdeba8c0c344ccb3c6a79110d9f988e7
Wilson <wilsongoh94@hotmail.com>
2019-02-25 16:13:12 +0800

    disabled color

--- app/src/main/java/com/pocketwallet/pocket/RequestActivity.java
+++ app/src/main/java/com/pocketwallet/pocket/RequestActivity.java
@@ -40,7 +40,7 @@ public class RequestActivity extends AppCompatActivity {
             System.out.println("Device does not support NFC");
             nfcCard.setClickable(false);
             nfcCard.setEnabled(false);
-            nfcCard.setCardBackgroundColor(Color.LTGRAY);
+            nfcCard.setAlpha(0.5f);
         }
 
         nfcCard.setOnClickListener(new View.OnClickListener() {
